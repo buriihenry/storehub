38d5c4b9bf56d47d0ca57cfd6dd2070a11c31917
buriihenry <buriihenry@gmail.com>
2025-01-07 14:03:30 +0300

    Validate product ID in deleteProduct and update server port configuration

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,13 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.use(express.json()); // This will allow us to parse JSON data.(middleware)fucntion that runs before the actual route.
 
 app.use("/api/products", productRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started on http://localhost:3000");
+    console.log("Server started on http://localhost:"+ PORT);
 });
