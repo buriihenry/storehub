57f719a0feb4a3270e496b42ff285d47ded3bce1
buriihenry <buriihenry@gmail.com>
2025-01-07 04:11:05 +0300

    Implement CRUD operations for products in the API

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // This will allow us to parse JSON data.(middleware)fucntion that runs before the actual route.
+
+app.get("/api/products", async(req, res) =>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success:true, data:products});
+    } catch (error) {
+        console.log("error in fetching products:", error.message);
+        
+    }
+})
 
 app.post("/api/products", async (req,res) =>{
     const product =req.body; // this is the data that we are sending to the server
@@ -26,6 +37,38 @@ app.post("/api/products", async (req,res) =>{
         res.status(500).json({success:false, message:"Server error."});
     }
 });
+
+app.put("/api/products/:id", async(req, res)=>{
+    const {id} =req.params;
+
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message:"Invalid Product Id"});
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success:true, data: updatedProduct});
+    } catch (error) {
+        res.status(500).json({success: false, message:"Server Error"});
+        
+    }
+})
+
+app.delete("/api/products/:id", async(req,res) =>{
+    const {id} = req.params;
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true, message:"Product Deleted"});
+    } catch (error) {
+        console.log("error in deleting product:", error.message);
+        res.status(404).json({success:false,message:"Product not found"});
+        
+    }
+    
+})
 
 app.listen(3000, () => {
     connectDB();
